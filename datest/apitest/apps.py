6c38172aad08017a9425576b61db111f1f65ad69
e <605662545@qq.com>
2021-03-05 14:57:29 +0800

    newcode

--- datest/apitest/apps.py
+++ datest/apitest/apps.py
@@ -4,7 +4,3 @@ from django.contrib.admin.apps import AdminConfig
 class ApitestConfig(AppConfig):
     name = 'apitest'
     verbose_name = 'api测试'
-
-
-class MyAdminConfig(AdminConfig):
-    default_site = 'apitest.admin.EventAdminSite'
--- datest/apitest/views.py
+++ datest/apitest/views.py
@@ -1,10 +1,13 @@
+import jmespath
 from django.shortcuts import render
+from jmespath.exceptions import JMESPathError
 from rest_framework import viewsets, renderers
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, action
 from .models import *
 from .serializers import *
 from .viewsfunc import *
+from httprunner.builtin import comparators
 # Create your views here.
 
 class PostdataViewset(viewsets.ModelViewSet):
@@ -22,5 +25,21 @@ class TestcaseViewset(viewsets.ModelViewSet):
     @action(methods=['get'],detail='testcase-detail',url_path='postcase',url_name='testcase-postcase')
     def runcase(self,request, *args, **kwargs):
         case = Testcase.objects.get(pk=kwargs['pk'])
-        rtext = apipost(httpMethod=case.api.method,endpoint=case.baseurl.url,requestUri=case.api.url, data=case.requestdata)
-        return Response(json.loads(rtext.text))
+        res = apipost(httpMethod=case.api.method,endpoint=case.baseurl.url,requestUri=case.api.url, data=case.requestdata)
+        resp_obj_meta = {
+            "status_code": res.status_code,
+            "headers": res.headers,
+            "cookies": res.cookies,
+            "body": json.loads(res.text,encoding='utf-8'),
+        }
+        try:
+            for assdata in case.assertparam_set.all():
+                check_value = jmespath.search(assdata.paramkey.value, resp_obj_meta)
+                expect_value = assdata.paramval.value
+                result = comparators.equal(check_value,expect_value)
+        except JMESPathError as ex:
+            return Response({'JMESPathError':str(ex)})
+        except AssertionError as e:
+            return Response({'assert':'失败','check_value':check_value,'expect_value':expect_value,'data':json.loads(case.requestdata,encoding='utf-8'),'response':json.loads(res.text,encoding='utf-8')})
+        result = {'assert':'成功','check_value':check_value,'expect_value':expect_value,'data':json.loads(case.requestdata,encoding='utf-8'),'response':json.loads(res.text,encoding='utf-8')}
+        return Response(result)
