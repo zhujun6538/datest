f19a96574d3d69950e0d68839cfe671e1c49a6ff
e <605662545@qq.com>
2021-03-23 20:43:38 +0800

    newcode

---
+++ datest/apitest/urls.py
@@ -0,0 +1,30 @@
+"""
+@project: PyCharm
+@author: MZM
+@file: urls.py
+@ide: PyCharm
+@time: 2021/3/18 17:19
+@desc： 
+"""
+from django.conf.urls import url,include
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from . import views
+from datest import settings
+
+def get_media_root():
+    return settings.MEDIA_ROOT
+
+router = DefaultRouter()
+router.APIRootView = views.RootView
+router.register(r'postdata',views.PostdataViewset)
+router.register(r'testcase',views.TestcaseViewset)
+router.register(r'testsuite',views.TestsuiteViewset)
+router.register(r'testbatch',views.TestbatchViewset)
+router.register(r'debugtalk',views.DebugTalkViewset)
+
+urlpatterns = [
+    url(r'^',include(router.urls)),
+    # path(r'runsuite/<int:id>/',views.runsuite),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+]
